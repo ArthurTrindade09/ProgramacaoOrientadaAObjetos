25401b908ec861c776135ee4b2c5d922faa16106
xX-Arthur-Trindade-Xx <arthurtrindade2009+@gmail.com>
2026-08-17 09:30:24 -0300

    Update Principal.java

--- POO/Proj_ExercicioReceberDados_170826/src/br/edu/principal/Principal.java
+++ POO/Proj_ExercicioReceberDados_170826/src/br/edu/principal/Principal.java
@@ -10,50 +10,50 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		String nome, rua, numero, bairro, complemento, cep, cidade, uf, cpf, data, idade;
+		String nome, rua, numero, bairro, complemento,
+			cep, cidade, uf, cpf, data, idade;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite seu nome: ");
-		nome = sc.next();
+		nome = sc.nextLine();
 		
 		System.out.println("Digite o nome de sua rua: ");
-		rua = sc.next();
+		rua = sc.nextLine();
 		
 		System.out.println("Digite o número da casa: ");
-		numero = sc.next();
+		numero = sc.nextLine();
 		
 		System.out.println("Digite o nome de seu bairro: ");
-		bairro = sc.next();
+		bairro = sc.nextLine();
 		
 		System.out.println("Digite seu complemento: ");
-		complemento = sc.next();
+		complemento = sc.nextLine();
 		
 		System.out.println("Digite seu cep: ");
-		cep = sc.next();
+		cep = sc.nextLine();
 		
 		System.out.println("Digite o nome de sua cidade: ");
-		cidade = sc.next();
+		cidade = sc.nextLine();
 		
 		System.out.println("Digite o uf: ");
-		uf = sc.next();
+		uf = sc.nextLine();
 		
 		System.out.println("Digite seu cpf: ");
-		cpf = sc.next();
+		cpf = sc.nextLine();
 		
 		System.out.println("Digite sua data de nascimento: ");
-		data = sc.next();
+		data = sc.nextLine();
 		
 		System.out.println("Digite sua idade: ");
-		idade = sc.next();
+		idade = sc.nextLine();
 		
 		sc.close();
 		
-		System.out.println("Nome: "+ nome);
-		System.out.println("Endereço: "+ rua+ ", "+ numero+ ", "+ bairro+ ", "+ complemento+ ", "+ cep+ ", "+ cidade+ ", "+ uf);
-		System.out.println("CPF: "+ cpf);
-		System.out.println("Data de Nascimento: "+ data);
-		System.out.println("Idade: "+ idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("Endereço: " + rua + ", " + numero + ", " + bairro + ", " + complemento + ", " + cep + ", " + cidade + ", " + uf);
+		System.out.println("CPF: " + cpf);
+		System.out.println("Data de Nascimento: " + data);
+		System.out.println("Idade: " + idade);
 	}
-
 }
