b8db4b379bad6322bb5c259f9bb04b543e4aa125
xX-Arthur-Trindade-Xx <arthurtrindade2009+@gmail.com>
2026-08-17 08:54:15 -0300

    Add files via upload

---
+++ POO/Proj_CalcMedia_170826/src/br/edu/principal/Principal.java
@@ -0,0 +1,37 @@
+
+// --------------------------------------------------
+// POO.
+// Calculadora de Média que recebe um nome e 2 notas.
+// Projeto aula 17/08/26.
+// --------------------------------------------------
+
+package br.edu.principal;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		Double n1, n2, media;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		nome = sc.next();
+		
+		System.out.println("Digite sua nota 01: ");
+		n1 = sc.nextDouble();
+		
+		System.out.println("Digite sua nota 02: ");
+		n2 = sc.nextDouble();
+		
+		media = (n1+n2)/2;
+		
+		sc.close();
+		
+		System.out.println("Nome: "+ nome);
+		System.out.println("Média: "+ media);
+	}
+
+}
